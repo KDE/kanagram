6c8057036c006c6d5e12f8105b048d23ab8afcf3
Joshua Keel <joshuakeel@gmail.com>
2005-07-03 10:12:05 +0000

    Fix compile error.
    svn path=/trunk/playground/edu/kanagram/; revision=431117

--- src/kanagramgame.cpp
+++ src/kanagramgame.cpp
@@ -47,7 +47,6 @@ void KanagramGame::nextAnagram()
 	m_anagram = createAnagram(m_originalWord);
 	m_hint = wordList[wordNumber].translatedText();
 
-	cout << "Title of the file: " << wordList[0].title();
 	cout << "Original word: " << m_originalWord << endl;
 }
 
