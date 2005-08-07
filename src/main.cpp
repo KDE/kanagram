78754da3910f6cd8b2cf8e9a049d82937a461ed8
Danny Allen <dannya@kde.org>
2005-08-07 13:51:22 +0000

    add artemio to app credits...
    svn path=/trunk/playground/edu/kanagram/; revision=443817

--- src/main.cpp
+++ src/main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
 	KAboutData about("kanagram", I18N_NOOP("Kanagram"), "0.1", I18N_NOOP("An anagram game"), KAboutData::License_GPL, "© 2005 Joshua Keel\n© 2005 Danny Allen");
 	about.addAuthor("Joshua Keel", I18N_NOOP("Coding"), "joshuakeel@gmail.com");
 	about.addAuthor("Danny Allen", I18N_NOOP("Design, Graphics and many Vocabularies"), "danny@dannyallen.co.uk");
+	about.addCredit("Artemiy Pavlov", I18N_NOOP("Sound effects"), 0, "http://artemiolabs.com");
 	KCmdLineArgs::init(argc, argv, &about);
 	KApplication app;
 	app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
