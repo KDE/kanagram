cbcdf07fa188b3bc0fb45a6d430db729c6294568
Joshua Keel <joshuakeel@gmail.com>
2005-10-01 17:50:17 +0000

    Forward port: Make KNewStuff work properly for all languages
    svn path=/trunk/KDE/kdeedu/kanagram/; revision=466142

---
+++ src/newstuffdialog.cpp
@@ -0,0 +1,33 @@
+#include "newstuffdialog.h"
+
+#include <knewstuff/knewstuff.h>
+#include <knewstuff/entry.h>
+#include <klocale.h>
+#include <kio/netaccess.h>
+#include <kurl.h>
+#include <kstandarddirs.h>
+
+#include "kanagramsettings.h"
+
+NewStuffDialog::NewStuffDialog() : KNewStuff("kanagram/vocabulary", "http://kde-edu.org/kanagram/" + KanagramSettings::dataLanguage() + ".xml")
+{
+}
+
+NewStuffDialog::~NewStuffDialog()
+{
+}
+
+bool NewStuffDialog::install(const QString &fileName)
+{
+}
+
+QString NewStuffDialog::downloadDestination(KNS::Entry *entry)
+{
+	QString fileName = entry->payload().path();
+	fileName = fileName.section('/', -1);
+	return KGlobal::dirs()->saveLocation("data", "kanagram/data/" + KanagramSettings::dataLanguage()) + fileName;
+}
+
+bool NewStuffDialog::createUploadFile(const QString &fileName)
+{
+}
