dc0aaebff71ced6fd9666523b0ffa86ba85b21f1
Dirk Mueller <mueller@kde.org>
2005-10-12 11:17:57 +0000

    false was probably more intended
    svn path=/trunk/KDE/kdeedu/kanagram/; revision=469870

--- src/newstuffdialog.cpp
+++ src/newstuffdialog.cpp
@@ -19,7 +19,7 @@ NewStuffDialog::~NewStuffDialog()
 
 bool NewStuffDialog::install(const QString &fileName)
 {
-		return true;
+  return false;
 }
 
 QString NewStuffDialog::downloadDestination(KNS::Entry *entry)
@@ -31,5 +31,5 @@ QString NewStuffDialog::downloadDestination(KNS::Entry *entry)
 
 bool NewStuffDialog::createUploadFile(const QString &fileName)
 {
-		return true;
+  return false;
 }
