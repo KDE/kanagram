f08f888f06c2bdbc5c9b9edda19c90dba0714d3b
Jeremy Paul Whiting <jpwhiting@kde.org>
2008-01-06 16:00:07 +0000

    added Ukranian translation credits
    svn path=/trunk/KDE/kdeedu/kanagram/; revision=757949

--- src/main.cpp
+++ src/main.cpp
@@ -41,6 +41,7 @@ int main(int argc, char *argv[])
     about.addCredit(ki18n("Eric Krüse"), ki18n("British English Data Files"), "bildvontux@yahoo.de");
     about.addCredit(ki18n("Hanna Scott"), ki18n("Swedish Data Files"), "hanna.et.scott@gmail.com");
     about.addCredit(ki18n("Jure Repinc"), ki18n("Slovenian Data Files"), "jlp@holodeck1.com");
+    about.addCredit(ki18n("Yuri Chornoivan"), ki18n("Ukranian Data Files"), "");
     KCmdLineArgs::init(argc, argv, &about);
     KApplication * app = new KApplication();
 
