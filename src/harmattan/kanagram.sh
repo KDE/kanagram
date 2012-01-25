a8e53e76d3b6748a41e312883ea5d0bffc16747b
Laszlo Papp <ext-laszlo.papp@nokia.com>
2012-01-26 01:14:11 +0200

    Use the 'e' type for the invoker for the script, and not the declarative switch

--- src/harmattan/kanagram.sh
+++ src/harmattan/kanagram.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-LD_LIBRARY_PATH=/opt/kanagram/lib /usr/bin/invoker --type=d -s /opt/kanagram/bin/kanagram-harmattan
+LD_LIBRARY_PATH=/opt/kanagram/lib /usr/bin/invoker --type=e -s /opt/kanagram/bin/kanagram-harmattan
