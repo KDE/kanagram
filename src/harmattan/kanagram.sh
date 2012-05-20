e3258954c7322e504c21ab535260ee567894825d
Laszlo Papp <lpapp@kde.org>
2012-05-20 03:14:10 +0300

    Establish splash screens for the harmattan version of kanagram

--- src/harmattan/kanagram.sh
+++ src/harmattan/kanagram.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-LD_LIBRARY_PATH=/opt/kanagram/lib /usr/bin/invoker --type=e -s /opt/kanagram/bin/kanagram-harmattan
+LD_LIBRARY_PATH=/opt/kanagram/lib /usr/bin/invoker --splash=/opt/kanagram/share/apps/kanagram-chalkboard-portrait-rotated.png --splash-landscape=/opt/kanagram/share/apps/kanagram-chalkboard-landscape.png --type=e -s /opt/kanagram/bin/kanagram-harmattan
