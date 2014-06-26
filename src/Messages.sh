ef390b5f4ff3f9022882517ef82f88565023f4f1
Luigi Toscano <luigi.toscano@tiscali.it>
2014-06-26 22:01:37 +0200

    Fix and simplify message extraction

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,8 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name "*.kcfg"` >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" -o -name "*.h"` -o $podir/kanagram.pot
-$XGETTEXT `find . -name \*.qml` -L Java -o $podir/kanagram.temp.pot
-$XGETTEXT `find src/ -name "*.cpp" -o -name "*.h"` -o $podir/kanagram.pot
-$MSGCAT $podir/kanagram.temp.pot $podir/kanagram.pot -o $podir/kanagram.pot
-rm -f $podir/kanagram.temp.pot
-
+$XGETTEXT `find . -name "*.cpp" -o -name "*.h" -o -name "*.qml"` -o $podir/kanagram.pot
