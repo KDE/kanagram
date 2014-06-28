1410c753c5a58b6b21805c1937b53edfea9b268d
Lasse Liehu <lasse.liehu@gmail.com>
2014-06-28 14:53:47 +0300

    Extract messages from new .ui files

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.kcfg"` >> rc.cpp
+$EXTRACTRC `find . -name "*.kcfg" -o -name "*.ui"` >> rc.cpp
 $XGETTEXT `find . -name "*.cpp" -o -name "*.h" -o -name "*.qml"` -o $podir/kanagram.pot
