714054961e24fd5f4e7d78497c86e1644ed7abbd
Sayan Biswas <techsayan01@gmail.com>
2015-04-27 10:47:42 +0530

    Credits for letters turned to clickable buttons

--- src/main.cpp
+++ src/main.cpp
@@ -68,6 +68,7 @@ int main(int argc, char **argv)
     about.addCredit(i18n("Yuri Chornoivan"), i18n("Ukranian Data Files"), "");
     about.addCredit(i18n("Kristóf Kiszel"), i18n("Hungarian Data Files"), "ulysses@kubuntu.org");
     about.addCredit(i18n("Souvik Das"), i18n("2-player mode"), "souvikdas728@gmail.com");
+    about.addCredit(i18n("Sayan Biswas"), i18n("Letters turned to clickable buttons"), "techsayan01@gmail.com");
 
     KAboutData::setApplicationData(about);
 
